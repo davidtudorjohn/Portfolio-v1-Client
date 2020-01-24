4c093fe66e68134e5d868f41a5706d8ac26e5410
David John <davidjohn_atl@yahoo.com>
2020-01-23 19:38:17 -0500

    removed unused import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import { Provider } from "react-redux";
 const store = createStore(rootReducer);
