882e5b138eb9f88e5718631b958fe777a652f856
David John <davidjohn_atl@yahoo.com>
2020-01-24 14:23:58 -0500

    removed return statement and changed filename

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.scss";
+const Footer = () => (
+  <div id="footerWrap">
+    <h1 id="logoFooter">DTJ</h1>
+    <p id="footerText">
+      Made with <strong>love</strong> in Atlanta
+    </p>
+  </div>
+);
+export default Footer;
