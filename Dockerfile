77f52ac18e37740b42b18def1ccf68e730cc5ed9
David John <davidjohn_atl@yahoo.com>
2020-02-19 11:57:18 -0500

    removed unused packages and imports

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:alpine
-WORKDIR '/usr/src/app'
+WORKDIR /usr/src/app/client
 COPY package*.json ./
-RUN npm install
+RUN npm install --verbose
 COPY . .
 EXPOSE 3000
 CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import { Provider } from "react-redux";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
