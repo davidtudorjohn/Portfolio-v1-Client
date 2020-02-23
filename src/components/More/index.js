e544b3c400ce9fc3a8cb1349b2befdcc28db129f
David John <davidjohn_atl@yahoo.com>
2020-02-23 14:05:59 -0500

    updated styles

--- src/components/More/index.js
+++ src/components/More/index.js
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 const More = () => {
   const isDarkMode = useSelector(state => state.isDarkMode);
   return (
-    <div id="moreWrap" className={isDarkMode && "dark"}>
+    <>
       <Link to="/">
         <div id="backBtn" className={isDarkMode && "dark"}>
           <FontAwesomeIcon
@@ -21,29 +21,28 @@ const More = () => {
           </p>
         </div>
       </Link>
-      <div id="moreCol1" className={isDarkMode && "dark"}>
-        <Feature feature="Routing" tool="React Router" />
-        <Feature feature="AJAX" tool="Fetch and axios" />
-        <Feature feature="REST API" tool="AJAX, Node.js and Express.js" />
-        <Feature
-          feature="State Management"
-          tool="Redux, Component State, and Context"
-        />
-        <Feature feature="Backend Framework" tool="Express.js" />
-        <Feature feature="Data Management" tool="MySQL and MongoDB" />
+      <div id="moreWrap" className={isDarkMode && "dark"}>
+        <div id="moreCol1" className={isDarkMode && "dark"}>
+          <Feature feature="Routing" tool="React Router" />
+          <Feature feature="AJAX" tool="Fetch and axios" />
+          <Feature feature="REST API" tool="AJAX, Node.js and Express.js" />
+          <Feature
+            feature="State Management"
+            tool="Redux, Component State, and Context"
+          />
+          <Feature feature="Backend Framework" tool="Express.js" />
+          <Feature feature="Data Management" tool="MySQL and MongoDB" />
+        </div>
+        <div id="moreCol2" className={isDarkMode && "dark"}>
+          <Feature feature="CSS Preprocessing" tool="Sass" />
+          <Feature feature="Package Management" tool="npm and yarn" />
+          <Feature feature="Version Control" tool="Git and Github" />
+          <Feature feature="Authentication" tool="bcryptjs and jsonwebtoken" />
+          <Feature feature="Deployment" tool="Docker, AWS and Webpack" />
+          <Feature feature="Unit Testing" tool="Jest" />
+        </div>
       </div>
-      <div id="moreCol2" className={isDarkMode && "dark"}>
-        <Feature feature="CSS Preprocessing" tool="Sass" />
-        <Feature feature="Package Management" tool="npm and yarn" />
-        <Feature feature="Version Control" tool="Git and Github" />
-        <Feature feature="Authentication" tool="bcryptjs and jsonwebtoken" />
-        <Feature feature="Deployment" tool="Docker, AWS and Webpack" />
-        <Feature feature="Unit Testing" tool="Jest" />
-        <br />
-        <br />
-        <br />
-      </div>
-    </div>
+    </>
   );
 };
 export default More;
